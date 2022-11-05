6c8468a5537c406a56ec22b35c72ddc9cd5f1f93
Pythadela <s.radomski19@gmail.com>
2022-11-05 23:49:28 +0100

    Added clipboard history and changed program icon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
 import tkinter as tk
 from tkinter import *
+
+import pyperclip
 
 BG_COLOR = "#e3e3e3"
 
@@ -8,15 +11,14 @@ class ClipboardManager:
     def __init__(self):
         self.window = tk.Tk()
         self.window.resizable(False, False)
-        scrollbar = Scrollbar(self.window)
+        self.window.iconbitmap("images/favicon.ico")
+
+        scrollbar = Scrollbar()
         scrollbar.pack(side=RIGHT, fill=Y)
+        self.mylist = Listbox(self.window, bg=BG_COLOR, width=24, yscrollcommand=scrollbar.set)
 
-        mylist = Listbox(self.window, bg=BG_COLOR, width=24, yscrollcommand=scrollbar.set)
-        for line in range(10):
-            mylist.insert(0, "")
-
-        mylist.pack(side=LEFT, fill=BOTH)
-        scrollbar.config(command=mylist.yview)
+        self.mylist.pack(side=LEFT, fill=BOTH)
+        scrollbar.config(command=self.mylist.yview)
 
     def place_window(self, width=165, height=115):
         screen_width = self.window.winfo_screenwidth()
@@ -26,6 +28,12 @@ class ClipboardManager:
         y = screen_height - (height + 73.5)
         self.window.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
+    def clip(self):
+        if not (pyperclip.waitForNewPaste() == ""):
+            clipboard = pyperclip.waitForPaste()
+            header = f" {clipboard}"
+            self.mylist.insert(0, header)
+
     def run(self):
         self.window.mainloop()
 
@@ -33,4 +41,13 @@ class ClipboardManager:
 if __name__ == "__main__":
     clipboard_manager = ClipboardManager()
     clipboard_manager.place_window()
+
+    def background():
+        while True:
+            clipboard_manager.clip()
+
+
+    b = threading.Thread(name='background', target=background)
+
+    b.start()
     clipboard_manager.run()
